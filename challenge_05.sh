5468d6b7ba647b57d9cc5f25c9ebfc5852de3d8a
lars <clausenlars@gmail.com>
2022-02-08 04:19:06 +0000

    challenge 05

---
+++ challenge_05.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Script Name: 201 Ops Challenge 5
+# Author: Lars Clausen
+# Date of last revision: 2/5/22
+
+# Displays running proccesses 
+
+ps aux
+
+# Asks user for PID
+
+echo "Give a proccess ID to terminate a process"
+read $PID
+
+# Ask user if yes/no if they want to proceed and kills process if yes.  
+
+echo "Are you sure you want to end this process?"
+options= 'Yes No'
+
+select option in $options
+do
+	if [ $option == 'Yes']
+	then
+		kill -15 $PID
+	fi
+	if [ $option == 'No' ]
+	then
+		break
+	fi
+done 
